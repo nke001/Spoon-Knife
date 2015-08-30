b50c05568001ee3a0d7b2cfef92a735267944f49
Nan Rosemary Ke <rosemary.ke@west.cmu.edu>
2015-08-29 21:38:24 -0400

    new commit

---
+++ Spoon/spoon.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+class TeaSpoon():
+    def __init__(self,msg):
+        print msg
---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from Spoon.spoon import TeaSpoon  
+
+def main():
+    ts = TeaSpoon('new TeaSpoon instance')
+
+if __name__ == '__main__':
+    main()
